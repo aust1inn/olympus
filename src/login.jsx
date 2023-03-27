f97a1572399af087487c45b5ddfdb24e9e7b8ede
aust1inn <austinotieno364@gmail.com>
2023-03-27 17:36:20 -0500

    feat:adds the UseHistory Functionality to redirect the user to the tags component

--- src/login.jsx
+++ src/login.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(email);
+        history.push('/Tags');
     }
 
     return (
@@ -17,7 +20,7 @@ export const Login = (props) => {
                 <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button  type="submit"  onClick={() => props.onFormSwitch('tag')}>Log In</button>
+                <button  type="submit">Log In</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
         </div>
