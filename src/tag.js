10c2835eabf922ababe7b05b83e6dd548a0a61b1
aust1inn <austinotieno364@gmail.com>
2023-03-28 09:59:24 -0500

    appropriately named the tags

--- src/tag.js
+++ src/tag.js
@@ -3,13 +3,13 @@ import React, { useState } from "react";
 export const Tags = (props) => {
     
     // create an array of tag options and initialize the selected tag state to null:
-    const tagOptions = ["None", "Red", "Orange", "Lime", "Green", "Blue", "Violet", "Magenta", "Gold"];
+    const tagOptions = ["None", "Warmup", "Working Set", "Drop Set", "Burnout", "Max Attempt", "Assisted", "Negatives"];
 
     const [selectedTag, setSelectedTag] = useState(null);
 
     // create a function to handle the selection of a tag:
 
-    
+
 
 
 
